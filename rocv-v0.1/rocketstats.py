b01e2490226afb4af76871312db0c12cb3fa3e78
Eric Yuan <yuanhaozhe2022@gmail.com>
2026-06-22 16:03:53 -0400

    Delete rocv-v0.1 directory

--- rocv-v0.1/rocketstats.py
+++
@@ -1,8 +0,0 @@
-class RocketStats:
-    def __init__(self,rocket):
-        self.rocket=rocket
-        self.mass=None
-        self.cg=None
-        self.cp=None
-        self.stability=None
-        self.max_altitude=None
